e37b793852304377628f8c22aeee2da1f6b389aa
Bacardii <ali.02.aboubacar@gmail.com>
2022-11-19 16:14:04 +0100

    database update

--- config/db.config.js
+++ config/db.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  HOST: db4free.net,
-  USER:'Bacardii',
+  HOST: 'db4free.net',
+  USER:'bacardii',
   PASSWORD: 'MG@Mw7aiC3VUcq@',
   DB: 'groupomania_db',
   dialect: "mysql",
+  port:3306,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
     idle: 10000,
-    port:3306,
   },
 };
